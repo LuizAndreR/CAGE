6e6082f8a5ade32f88f6143121f396f8cc89a54c
LuizAndreR <122027228+LuizAndreR@users.noreply.github.com>
2026-08-25 19:25:52 -0300

    feat(receitas): adiciona exibicao de custo extra e corrige localidade pt-BR

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,10 +6,16 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { authInterceptor } from './core/auth/auth.interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimationsAsync()
   ]
