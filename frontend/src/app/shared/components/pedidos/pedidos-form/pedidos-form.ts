67a230ab3065e8121442dde8654e1951209356b7
LuizAndreR <122027228+LuizAndreR@users.noreply.github.com>
2026-09-08 11:36:35 -0300

    feat(form): criar estrutura inicial do componente form

---
+++ frontend/src/app/shared/components/pedidos/pedidos-form/pedidos-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-form',
+  imports: [],
+  templateUrl: './pedidos-form.html',
+  styleUrl: './pedidos-form.css',
+})
+export class PedidosForm {}
